2cbd32e2d8eb0223995d23f754d5cd490e2a2f81
JMoneymaker <jasmin.moneymaker@gmail.com>
2020-04-21 17:50:13 -0700

    ready for test seed

--- __tests__/dataShapers.test.js
+++ __tests__/dataShapers.test.js
@@ -17,6 +17,13 @@ describe('bail numifier', () => {
     expect(numifyBail(input)).toEqual(output);
   });
 
+  test.only('it returns a number if the value is a string with a ,', () => {
+    const input = '$2,500';
+    const output = 2500;
+
+    expect(numifyBail(input)).toEqual(output);
+  });
+
   test('it returns a number if the value is a string with $0', () => {
     const input = '$0';
     const output = 0;
