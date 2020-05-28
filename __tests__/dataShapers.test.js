87fd74701288b9dd6d434101527e638d2351cc69
JMoneymaker <jasmin.moneymaker@gmail.com>
2020-05-28 12:37:08 -0700

    add time to multnomah booking date

--- __tests__/dataShapers.test.js
+++ __tests__/dataShapers.test.js
@@ -1,4 +1,4 @@
-const { makeDate, numifyBail, getAge, makeDateWash } = require('../lib/utils/dataShapers.js');
+const { makeDate, makeDateWithTime, numifyBail, getAge, makeDateWash } = require('../lib/utils/dataShapers.js');
 const moment = require('moment');
 moment().format();
 
@@ -69,7 +69,44 @@ describe('date maker', () => {
   });
 });
 
-describe.only('washington date maker', () => {
+describe.only('date and time maker', () => {
+  it('returns correct time for PM', () => {
+    const input = '05/21/2020 10:31 PM';
+    const output = '2020-05-21T22:31:00-07:00';
+
+    expect(makeDateWithTime(input)).toEqual(output);
+  });
+
+  it('returns correct time for AM', () => {
+    const input = '05/21/2020 10:31 AM';
+    const output = '2020-05-21T10:31:00-07:00';
+
+    expect(makeDateWithTime(input)).toEqual(output);
+  });
+
+  it('returns correct time with missing numbers', () => {
+    const input = '05/21/2020 1:31 AM';
+    const output = '2020-05-21T01:31:00-07:00';
+
+    expect(makeDateWithTime(input)).toEqual(output);
+  });
+
+  it('returns correct date with no time', () => {
+    const input = '05/21/2020';
+    const output = '2020-05-21T00:00:00-07:00';
+
+    expect(makeDateWithTime(input)).toEqual(output);
+  });
+
+  it('returns undefined if the date is an empty string', () => {
+    const input = ' ';
+    const output = undefined;
+
+    expect(makeDateWithTime(input)).toEqual(output);
+  });
+});
+
+describe('washington date maker', () => {
   it('returns undefined if the date is an empty string', () => {
     const input = ' ';
     const output = undefined;
