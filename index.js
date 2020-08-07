952dcf8439bf4dc80a41210b0141c0ab94ac3e7f
JMoneymaker <jasmin.moneymaker@gmail.com>
2020-08-06 21:38:58 -0700

    all counties working

--- index.js
+++ index.js
@@ -1,18 +1,42 @@
-const { multIdQueue, multRawDetentionQueue, multParsedDetentionQueue } = require('./lib/jobs/queue.js');
+const {
+  clackQueryQueue, 
+  clackScraperQueue, 
+  clackParserQueue, 
+  multQueryQueue, 
+  multScraperQueue, 
+  multParserQueue, 
+  washScraperQueue, 
+  washParserQueue  
+} = require('./lib/jobs/queue.js');
 
+const washQueries = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 
 Promise.all([
-  multIdQueue.empty(),
-  multRawDetentionQueue.empty(),
-  multParsedDetentionQueue.empty()
+  clackQueryQueue.empty(),
+  clackScraperQueue.empty(),
+  clackParserQueue.empty(),
+  multQueryQueue.empty(),
+  multScraperQueue.empty(),
+  multParserQueue.empty(),
+  washScraperQueue.empty(),
+  washParserQueue.empty()
 ])
-  .then(() => multIdQueue.add({}))
-  .then(() => console.log('Jobs added'))
+  .then(() => clackQueryQueue.add({}))
+  .then(() => console.log('Clackamas County jobs added'))
+  .then(() => multQueryQueue.add({}))
+  .then(() => console.log('Multnomah County jobs added'))
+  .then(() => Promise.all(washQueries.map(id => washScraperQueue.add({ id }))))
+  .then(() => console.log('Washington County jobs added'))
   .catch(error => console.log('Error adding jobs', error))
   .finally(() => Promise.all(([
-    multIdQueue.close(),
-    multRawDetentionQueue.close(),
-    multParsedDetentionQueue.close()
+    clackQueryQueue.close(),
+    clackScraperQueue.close(),
+    clackParserQueue.close(),
+    multQueryQueue.close(),
+    multScraperQueue.close(),
+    multParserQueue.close(),
+    washScraperQueue.close(),
+    washParserQueue.close()
   ])));
 
 
